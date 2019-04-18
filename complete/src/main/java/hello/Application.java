ad3fee9e5f90b5a377b31332348e4ed4b020aa8d
Weijing Lin <weijingjaylin@gmail.com>
2019-04-18 15:05:25 -0700

    1. change port to 8888 2. cors free

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,11 +2,26 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        // SpringApplication app = new SpringApplication(Application.class);
+        // app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
